d6b9bca5334dc071970fa5827cc82d5ab02c79ee
cinfacol <cinfacol@gmail.com>
2023-06-24 15:38:50 -0500

    Add Custom User Model

--- config/urls.py
+++ config/urls.py
@@ -6,3 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("supersecret/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Last Shop Admin"
+admin.site.site_title = "Last Shop Admin Portal"
+admin.site.index_title = "Welcome to the Last Shop Portal"
