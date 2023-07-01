2f9759273223cf2957e8e6f7748e25e5b5cd2cbf
cinfacol <cinfacol@gmail.com>
2023-07-01 14:51:22 -0500

    setup nginx, serve staticfiles

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # last-shop
 
 tienda virtual con Django, Next, Docker, Celey, Redis y Nginx
+
+(video 2 1:33:58)
--- config/settings.py
+++ config/settings.py
@@ -89,6 +89,21 @@ DATABASES = {
     }
 }
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8080",
+    "http://127.0.0.1:3000",
+]
 
 AUTH_PASSWORD_VALIDATORS = [
     {
